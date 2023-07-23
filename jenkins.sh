ad549b0876115c00b7f7781fafd9366883a48419
judexman01 <71095187+judexman01@users.noreply.github.com>
2023-07-23 05:02:52 +0100

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -8,6 +8,7 @@ sudo apt-get install openjdk-11-jdk -y
 
 sudo ufw allow 8080
 sudo ufw --force enable
+echo "y" | sudo ufw enable
 echo "install Jenkins"
 sudo curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
@@ -17,4 +18,3 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins.service
-sudo systemctl status jenkins
